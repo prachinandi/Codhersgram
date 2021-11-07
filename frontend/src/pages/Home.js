92e6b1cdc09fb39a36170af37c2bf63f6aad0bcd
Sambit Sankalp <82284130+sambit-sankalp@users.noreply.github.com>
2021-11-08 01:47:18 +0530

    feat:context added (#14)

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -25,7 +25,7 @@ const Home = () => {
             <div className="profile__name">Hemant Bajaj</div>
             <div className="profile__position">Senior software developer</div>
           </div>
-          <Button name="View Profile" src="profile" />
+          <Button name="View Profile" src="/profile" />
           <h2>Suggested Users</h2>
           <div className="profile__user__suggestions">
             <HomeSuggestedUserCard />
