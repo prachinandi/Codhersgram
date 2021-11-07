8ec417f80583da13db5b7c05b9981a851f38fe35
abhibhaw <39991296+abhibhaw@users.noreply.github.com>
2021-11-07 14:41:54 +0530

    feat: Add story,user,project query and mutations

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,8 +1,32 @@
 const User = require("./models/user.model");
+const Project = require("./models/project.model");
+const Story = require("./models/story.model");
 
 const resolvers = {
   Query: {
-    users: async () => User.find({}),
+    users: async () => {
+      return User.find({});
+    },
+    projects: async () => {
+      return Project.find({});
+    },
+    stories: async () => {
+      return Story.find({});
+    },
+  },
+  Mutation: {
+    addUser: (_, args) => {
+      let user = new User(args);
+      return user.save();
+    },
+    addProject: (_, args) => {
+      let project = new Project(args);
+      return project.save();
+    },
+    addStory: (_, args) => {
+      let story = new Story(args);
+      return story.save();
+    },
   },
 };
 
