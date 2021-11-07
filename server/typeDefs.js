75dc93aab463b181eea3a01f5df14b296855a852
Abhibhaw Asthana <39991296+abhibhaw@users.noreply.github.com>
2021-11-07 14:43:12 +0530

    feat: Add story,user,project query and mutations (#5)

--- server/typeDefs.js
+++ server/typeDefs.js
@@ -2,6 +2,7 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type User {
+    id: ID!
     name: String!
     uid: String!
     email: String
@@ -11,9 +12,45 @@ const typeDefs = gql`
     github: String
     followingIDs: [String]
     followersIDs: [String]
+    projects: [Project]
+  }
+  type Project {
+    id: ID!
+    name: String!
+    desc: String
+    link: String
+    userID: String!
+    imageURL: String!
+  }
+  type Story {
+    id: ID!
+    link: String
+    userID: String!
+    imageURL: String!
   }
   type Query {
     users: [User]
+    projects: [Project]
+    stories: [Story]
+  }
+  type Mutation {
+    addUser(
+      name: String!
+      uid: String!
+      email: String
+      bio: String
+      avatar: String
+      url: String
+      github: String
+    ): User
+    addProject(
+      name: String!
+      userID: String!
+      imageURL: String!
+      desc: String
+      link: String
+    ): Project
+    addStory(userID: String!, imageURL: String!, link: String): Story
   }
 `;
 
